f2e90c0c376e5f221c580fc7183395aa0ed0d108
nevita <triyanevita7@gmail.com>
2024-10-31 13:25:34 +0700

    percobaan 4 (rata nilai)

--- RataNilai21.java
+++ RataNilai21.java
@@ -3,15 +3,18 @@ public class RataNilai21 {
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i,j ;
-        float nilai,totalNilai,rataNilai;
+        float nilaiMhs,totalNilai,rataNilai;
 
         for (i = 1; i <= 5; i++) {    
+            System.out.println("Input Nilai Mahasiswa ke " + i);
+            totalNilai = 0;
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Nilai ke-" + j + " = ");
+                nilaiMhs = sc.nextInt();
+                totalNilai += nilaiMhs; 
+            }
+            rataNilai = totalNilai/5;
+            System.out.println("Rata-rata Nilai Mahasiswa ke " + i + " adalah " + rataNilai);  
         }
-        totalNilai = 0;
-        for (j = 1; j <= 5; j++) {
-            
-        }
-
-     }
-    
+     }   
 }
