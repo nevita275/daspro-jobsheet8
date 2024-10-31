d6d45e3feae7cfc11317c9daaf7082f4ebe0d6ff
nevita <triyanevita7@gmail.com>
2024-10-31 12:09:24 +0700

    percobaan 3 (Triangle)

---
+++ RataNilai21.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class RataNilai21 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,j ;
+        float nilai,totalNilai,rataNilai;
+
+        for (i = 1; i <= 5; i++) {    
+        }
+        totalNilai = 0;
+        for (j = 1; j <= 5; j++) {
+            
+        }
+
+     }
+    
+}
